d2084b159731c85929771d6812b3c91600c6110b
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-08 16:26:50 -0800

    add data to databasse on create

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -11,5 +11,18 @@ namespace Persistence
 
         }
         public DbSet<Value> Values {get;set;}
+
+        //protected can access in defined class and any derived classes from this class
+        //override OnModelCreating with the following 
+        //when new table is created add the following values
+        protected override void OnModelCreating(ModelBuilder builder){
+            builder.Entity<Value>()
+            .HasData(
+                new Value {Id = 1, Name = "Value 101"},
+                new Value {Id = 2, Name = "Value 102"},
+                new Value {Id = 3, Name = "Value 103"}
+
+            );
+        }
     }
 }
--- Readme.md
+++ Readme.md
@@ -4,3 +4,4 @@ dotnet watch run
 //script to run initial migration
 dotnet ef migrations add InitialCreate -p Persistence/ -s API/
 
+ dotnet ef migrations add Seedvalues -p Persistence/ -s API/
