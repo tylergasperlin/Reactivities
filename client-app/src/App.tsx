1578007cfc4a840969e13e7a05e9ccdb7ad561be
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-08 17:13:55 -0800

    test rendering data in appjs

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -3,11 +3,25 @@ import logo from './logo.svg';
 import './App.css';
 
 const App: React.FC = () => {
+  const [values, updateValues] = React.useState([{id: 1, name: 'Value 101'}])
+
+  React.useEffect(() => {
+    updateValues([{id: 1, name: 'Value 101'}, {id:2, name: 'Value 102'}])
+  })
+
   return (
-    <div>
-      <ul>
-        nice
-      </ul>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <ul>
+          {values.map((value: any)=>{
+            return(
+              <li>{value.name}</li>
+
+            )
+          })}
+        </ul>
+      </header>
     </div>
   );
 }
