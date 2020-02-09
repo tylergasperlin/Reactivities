6a619c3dd925c02877f02260b3c44357a28cbee1
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-08 22:53:43 -0800

    build out list

---
+++ Application/Activities/List.cs
@@ -0,0 +1,32 @@
+
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+//MediatR allows our api controllers to get data from the database 
+namespace Application.Activities
+{
+    public class List
+    {
+        public class Query : IRequest<List<Activity>> { }
+        public class Handler : IRequestHandler<Query, List<Activity>>
+        {
+            private readonly DataContext _context;
+            public Handler(DataContext context)
+            {
+                _context = context;
+
+            }
+            public async Task<List<Activity>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var activities = await _context.Activities.ToListAsync();
+
+                return activities;
+            }
+        }
+    }
+}
--- Readme.md
+++ Readme.md
@@ -5,3 +5,13 @@ dotnet watch run
 dotnet ef migrations add InitialCreate -p Persistence/ -s API/
 
  dotnet ef migrations add Seedvalues -p Persistence/ -s API/
+
+ MediatR
+    Takes object in > Handler > object out
+    Flow in app: Controller > mediaror.send(object) > object > handler > object > Controller
+    Command Handler: Activity goes in and Object comes out with some data about a successful save 
+    Query Handler: Object in to retrive by id > object about the requested object is the object out as a dto 
+
+ Clean Architecture
+ Devices/DB/UI/Web > Gateways/Controllers/Presenters > UseCases - Appliction Business Rules > Entities - Enterprise Busienss Rules
+
