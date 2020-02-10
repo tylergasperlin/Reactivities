89bad4e7cac74bcf7a177ac142650af858c3db7c
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-09 21:42:08 -0800

    cleanup

---
+++ client-app/src/app/interfaces/iActivity.ts
@@ -0,0 +1,11 @@
+
+//use interface when you want typechecking only //othersise class is appropriate
+export interface iActivity {
+    id: string;
+    title: string;
+    description: string;
+    category: string;
+    date: string;
+    city: string;
+    venue: string;
+}
--- client-app/src/app/layout/App.tsx
+++ client-app/src/app/layout/App.tsx
@@ -3,21 +3,12 @@ import './App.css';
 import axios, { AxiosResponse } from 'axios';
 import React from 'react';
 import { Header, Icon, List } from 'semantic-ui-react';
-
-interface iData {
-    id: string;
-    title: string;
-    description: string;
-    category: string;
-    date: string;
-    city: string;
-    venue: string;
-}
+import {iActivity} from '../interfaces/iActivity'
 
 
 
 const App: React.FC = () => {
-    const initialState: iData = {
+    const initialState: iActivity = {
         id: '',
         title: '',
         description: '',
@@ -27,13 +18,12 @@ const App: React.FC = () => {
         venue: ''
     }
 
-    const [activities, updateActivities] = React.useState([
+    const [activities, updateActivities] = React.useState<iActivity[]>([
         initialState
     ]);
 
     const getData = async() => {
-        const response: AxiosResponse<iData[]> = await axios.get('http://localhost:5000/api/activities')
-        console.log(response.data)
+        const response: AxiosResponse<iActivity[]> = await axios.get('http://localhost:5000/api/activities')
         updateActivities([...response.data])
     };
 
@@ -48,8 +38,8 @@ const App: React.FC = () => {
                 <Header.Content>Reactivities</Header.Content>
             </Header>
             <List>
-                {activities.map((activities: iData) => {
-                return <List.Item key={activities.id}>{activities.title}, {activities.description}: {activities.venue}</List.Item>;
+                {activities.map((activity: iActivity) => {
+                return <List.Item key={activity.id}>{activity.title}, {activity.description}: {activity.venue}</List.Item>;
                 })}
             </List>
         </div>
