1a358ab5ed5ac3e625ae62ce5125640c1cd509be
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-15 10:22:39 -0800

    Add sleeper to client requests

--- client-app/src/app/api/agent.ts
+++ client-app/src/app/api/agent.ts
@@ -1,24 +1,50 @@
 import axios, { AxiosResponse } from 'axios';
 import { iActivity } from '../interfaces/iActivity';
- axios.defaults.baseURL = 'http://localhost:5000/api'
+import { resolve } from 'dns';
+axios.defaults.baseURL = 'http://localhost:5000/api';
 
- const responseBody = (response: AxiosResponse) => response.data;
+const responseBody = (response: AxiosResponse) => response.data;
 
- const requests = {
-    get: (url: string)    => axios.get(url).then(responseBody),
-    post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
-    put: (url: string, body: {}) => axios.put(url, body).then(responseBody),
-    delete: (url: string) => axios.delete(url).then(responseBody)
- }
+const sleep = (ms: number) => (response: AxiosResponse) => {
+    return new Promise<AxiosResponse>(resolve =>
+        setTimeout(() => resolve(response), ms)
+    );
+};
 
- const Activities = {
-     list: (): Promise<iActivity[]> => requests.get('/activities'),
-     details: (id: string) => requests.get(`/activities/${id}`),
-     create: (activity: iActivity) => requests.post(`/activities`, activity),
-     update: (activity: iActivity) => requests.put(`/activities/${activity.id}`, activity),
-     delete: (id: string) => requests.delete(`/activities/${id}`)
- }
+const sleepTime = 1000;
 
- export default {
-     Activities
- }
+const requests = {
+    get: (url: string) =>
+        axios
+            .get(url)
+            .then(sleep(sleepTime))
+            .then(responseBody),
+    post: (url: string, body: {}) =>
+        axios
+            .post(url, body)
+            .then(sleep(sleepTime))
+            .then(responseBody),
+    put: (url: string, body: {}) =>
+        axios
+            .put(url, body)
+            .then(sleep(sleepTime))
+            .then(responseBody),
+    delete: (url: string) =>
+        axios
+            .delete(url)
+            .then(sleep(sleepTime))
+            .then(responseBody)
+};
+
+const Activities = {
+    list: (): Promise<iActivity[]> => requests.get('/activities'),
+    details: (id: string) => requests.get(`/activities/${id}`),
+    create: (activity: iActivity) => requests.post(`/activities`, activity),
+    update: (activity: iActivity) =>
+        requests.put(`/activities/${activity.id}`, activity),
+    delete: (id: string) => requests.delete(`/activities/${id}`)
+};
+
+export default {
+    Activities
+};
