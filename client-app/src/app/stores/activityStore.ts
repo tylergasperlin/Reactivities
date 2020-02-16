d2f03ec1fbd456752a5b21ac9897e53cee2d285f
Tyler Gasperlin <gasperlint@gmail.com>
2020-02-15 16:22:14 -0800

    Add map in the store instead of working with array

--- client-app/src/app/stores/activityStore.ts
+++ client-app/src/app/stores/activityStore.ts
@@ -4,15 +4,18 @@ import { IActivity } from '../interfaces/IActivity'
 import agent from '../api/agent';
 
 class ActivityStore {
+    //es6 map  = react to change in entry and additiona and removal
+    //allows you to easily turn arra into object
+    @observable activityRegistry = new Map()
     @observable activities: IActivity[] = []
     @observable selectedActivity: IActivity | undefined;
     @observable loadingInitial = false;
     @observable editMode = false;
     @observable submitting = false;
-
     //put dates in order
     @computed get activitiesByDate() {
-        return this.activities.sort((a,b) => Date.parse(a.date) - Date.parse(b.date))
+        //Array from values returns an iterable of values from the map
+        return Array.from(this.activityRegistry.values()).sort((a,b) => Date.parse(a.date) - Date.parse(b.date))
     }
 
     //it is ok to modify state in mobx and common to
@@ -21,13 +24,14 @@ class ActivityStore {
         const activityList  = await agent.Activities.list();
         activityList.forEach(activity => {
             activity.date = activity.date.split('.')[0];
-            this.activities.push(activity)
+            //turns the array into an object with key activity.id 
+            this.activityRegistry.set(activity.id, activity)
         });
         this.loadingInitial = false
     }
 
     @action selectActivity = (id: string) => {
-        this.selectedActivity = this.activities.find(a=> a.id === id);
+        this.selectedActivity = this.activityRegistry.get(id)
         this.editMode = false;
     }
 
@@ -35,7 +39,7 @@ class ActivityStore {
         this.submitting = true;
         try{
             await agent.Activities.create(activity)
-            this.activities.push(activity)
+            this.activityRegistry.set(activity.id, activity)
             this.editMode = false;
             this.submitting = false
         } catch (error) {
