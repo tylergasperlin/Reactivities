fe0b21d5ef34fdaf464c9a90a69e379c15f8959b
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-25 20:28:30 -0700

    Buildout jwt generator class

--- Infrastructure/Security/JwtGenerator.cs
+++ Infrastructure/Security/JwtGenerator.cs
@@ -1,5 +1,11 @@
+using System.Collections.Generic;
+using System.Security.Claims;
 using Application.Interfaces;
 using Domain;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+using Microsoft.IdentityModel.Tokens;
+using System;
 
 namespace Infrastructure.Security
 {
@@ -7,7 +13,27 @@ namespace Infrastructure.Security
     {
         public string CreateToken(AppUser user)
         {
-            throw new System.NotImplementedException();
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            //Generate signing credentials
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("Super secret key"));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature); //the strongest algorithm there is
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(7),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
         }
     }
 }
