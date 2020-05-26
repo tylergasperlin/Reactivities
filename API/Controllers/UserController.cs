49f6f8b8b186653853b00496848653e6f5b47f70
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-26 06:14:23 -0700

    Create register endpoint

--- API/Controllers/UserController.cs
+++ API/Controllers/UserController.cs
@@ -6,13 +6,19 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers
 {
+    [AllowAnonymous]
     public class UserController : BaseController
     {
-        [AllowAnonymous]
         [HttpPost("login")]
         public async Task<ActionResult<User>> Login(Login.Query query)
         {
             return await Mediator.Send(query);
+        }
+
+        [HttpPost("register")]
+        public async Task<ActionResult<User>> Register(Register.Command command)
+        {
+            return await Mediator.Send(command);
         }
     }
 }
