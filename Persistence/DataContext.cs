a2f08535da9982511a4883f1302167b18f262125
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-25 18:44:27 -0700

    Add user tables from identitycore

--- Persistence/DataContext.cs
+++ Persistence/DataContext.cs
@@ -1,10 +1,11 @@
 ﻿using System;
 using Domain;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace Persistence
 {
-    public class DataContext : DbContext
+    public class DataContext : IdentityDbContext<AppUser>
     {
         public DataContext(DbContextOptions options) : base(options)
         {
@@ -18,6 +19,8 @@ namespace Persistence
         //when new table is created add the following values
         protected override void OnModelCreating(ModelBuilder builder)
         {
+            base.OnModelCreating(builder);
+
             builder.Entity<Value>()
             .HasData(
                 new Value { Id = 1, Name = "Value 101" },
