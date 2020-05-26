43d327ed70f5ba9fd1a7c2ff3b8c04e279382f90
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-25 20:00:57 -0700

    Add a user object to return to client on login

--- Application/User/Login.cs
+++ Application/User/Login.cs
@@ -12,7 +12,7 @@ namespace Application.User
 {
     public class Login
     {
-        public class Query : IRequest<AppUser>
+        public class Query : IRequest<User>
         {
             public string Email { get; set; }
             public string Password { get; set; }
@@ -27,7 +27,7 @@ namespace Application.User
             }
 
         }
-        public class Handler : IRequestHandler<Query, AppUser>
+        public class Handler : IRequestHandler<Query, User>
         {
             private readonly UserManager<AppUser> _userManager;
             private readonly SignInManager<AppUser> _signInManager;
@@ -38,7 +38,7 @@ namespace Application.User
 
             }
             //cancellation request - if user refreshes or aborts their request what happends
-            public async Task<AppUser> Handle(Query request, CancellationToken cancellationToken)
+            public async Task<User> Handle(Query request, CancellationToken cancellationToken)
             {
                 var user = await _userManager.FindByEmailAsync(request.Email);
 
@@ -49,7 +49,13 @@ namespace Application.User
 
                 if (result.Succeeded)
                 {
-                    return user;
+                    return new User
+                    {
+                        DisplayName = user.DisplayName,
+                        Token = "This will be a token someday",
+                        UserName = user.UserName,
+                        Image = null
+                    };
                 }
 
                 throw new RestException(HttpStatusCode.Unauthorized);
