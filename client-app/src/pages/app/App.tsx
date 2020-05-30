e3bd6f8b51597cf768f128e424385057ef32b5c8
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-30 08:54:13 -0700

    Create login form

--- client-app/src/pages/app/App.tsx
+++ client-app/src/pages/app/App.tsx
@@ -1,12 +1,7 @@
 import './App.css';
 import { observer } from 'mobx-react-lite';
 import React from 'react';
-import {
-    Route,
-    RouteComponentProps,
-    withRouter,
-    Switch
-} from 'react-router-dom';
+import { Route, RouteComponentProps, withRouter, Switch } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 import ActivityDashboard from '../../components/activity-dashboard/ActivityDashboard';
 import ActivityDetails from '../../components/activity-details/ActivityDetails';
@@ -14,12 +9,13 @@ import ActivityForm from '../../components/activity-form/ActivityForm';
 import NavBar from '../../components/nav/NavBar';
 import { HomePage } from '../HomePage';
 import NotFound from '../../components/not-found/NotFound';
-import {ToastContainer} from 'react-toastify';
+import { ToastContainer } from 'react-toastify';
+import LoginForm from '../../components/login/LoginForm';
 
 const App: React.FC<RouteComponentProps> = ({ location }) => {
     return (
         <React.Fragment>
-            <ToastContainer position='bottom-right'/>
+            <ToastContainer position='bottom-right' />
             <Route exact path={'/'} component={HomePage} />
             <Route
                 // when we git route with forward slash and anything else return the following
@@ -29,16 +25,8 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                         <NavBar />
                         <Container style={{ marginTop: '7em' }}>
                             <Switch>
-                                <Route
-                                    exact
-                                    path={'/activities'}
-                                    component={ActivityDashboard}
-                                />
-                                <Route
-                                    exact
-                                    path={'/activities/:id'}
-                                    component={ActivityDetails}
-                                />
+                                <Route exact path={'/activities'} component={ActivityDashboard} />
+                                <Route exact path={'/activities/:id'} component={ActivityDetails} />
                                 {/* can use array to specify two paths for same component */}
                                 <Route
                                     exact
@@ -46,6 +34,7 @@ const App: React.FC<RouteComponentProps> = ({ location }) => {
                                     path={['/createActivity', '/manage/:id']}
                                     component={ActivityForm}
                                 />
+                                <Route path='/login' component={LoginForm} />
                                 <Route component={NotFound} />
                             </Switch>
                         </Container>
