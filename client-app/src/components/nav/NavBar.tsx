288e73e43d0b56e6dfd7d619f2eee8f7d23c0fff
Tyler Gasperlin <gasperlint@gmail.com>
2020-05-30 21:28:26 -0700

    Add login register to home

--- client-app/src/components/nav/NavBar.tsx
+++ client-app/src/components/nav/NavBar.tsx
@@ -1,19 +1,19 @@
 import { observer } from 'mobx-react-lite';
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { Button, Container, Menu } from 'semantic-ui-react';
+import React, { useContext } from 'react';
+import { NavLink, Link } from 'react-router-dom';
+import { Button, Container, Menu, Dropdown, Image } from 'semantic-ui-react';
+import { RootStoreContext } from '../../app/stores/rootStore';
 
 const NavBar: React.FC = () => {
+    const rootStore = useContext(RootStoreContext);
+    const { isLoggedIn, user } = rootStore.userStore;
+
     return (
         <Menu fixed='top' inverted>
             <Container>
                 {/* exact makes it so that each button is only highlighted based on current route */}
                 <Menu.Item header as={NavLink} to='/' exact>
-                    <img
-                        src='/assets/logo.png'
-                        alt='logo'
-                        style={{ marginRight: '10px' }}
-                    />
+                    <img src='/assets/logo.png' alt='logo' style={{ marginRight: '10px' }} />
                     Reactivities
                 </Menu.Item>
                 <Menu.Item name='Activities' as={NavLink} to='/activities' />
@@ -25,6 +25,22 @@ const NavBar: React.FC = () => {
                         content='Create Activity'
                     ></Button>
                 </Menu.Item>
+                {user && (
+                    <Menu.Item position='right'>
+                        <Image avatar spaced='right' src={user.image || '/assets/user.png'} />
+                        <Dropdown pointing='top left' text={user.displayName}>
+                            <Dropdown.Menu>
+                                <Dropdown.Item
+                                    as={Link}
+                                    to={`/profile/username`}
+                                    text='My profile'
+                                    icon='user'
+                                />
+                                <Dropdown.Item text='Logout' icon='power' />
+                            </Dropdown.Menu>
+                        </Dropdown>
+                    </Menu.Item>
+                )}
             </Container>
         </Menu>
     );
